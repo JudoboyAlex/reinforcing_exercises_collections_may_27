235c1ee59f71baeff21ee24b9cc9801ec050fae9
Alexander G. Yang <spark8402@rogers.com>
2019-05-26 22:29:32 -0400

    assignment done

---
+++ exercise.py
@@ -0,0 +1,13 @@
+digits = ['1','2','3','4','5','6','7','8','9']
+en = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+fr = ['un','deux','trois','quatre','cinq','six','sept','huit','neuf']
+
+
+
+new_dict = {}
+for i in range(len(digits)):
+    new_dict[digits[i]] = { 'french': fr[i], 'english': en[i]}
+print(new_dict)
+
+
+
